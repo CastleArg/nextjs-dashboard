bc56e3c59bc381612d6cefa7952be544b828c236
mark churcher <markchurcher@gmail.com>
2025-08-09 11:23:55 +1200

    individual skeletons

---
+++ app/ui/dashboard/card-wrapper.tsx
@@ -0,0 +1,14 @@
+import { Card } from '@/app/ui/dashboard/cards';
+import { fetchCardData } from '@/app/lib/data';
+
+export default async function CardWrapper() {
+  const { numberOfInvoices, numberOfCustomers, totalPendingInvoices, totalPaidInvoices } = await fetchCardData();
+  return (
+    <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Pending" value={totalPendingInvoices} type="pending" />
+      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+      <Card title="Total Customers" value={numberOfCustomers} type="customers" />
+    </div>
+  );
+}
